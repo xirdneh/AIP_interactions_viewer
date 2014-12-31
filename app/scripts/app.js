4c09a2012e56eba86f3ed153069259740a09cb01
Asher <asher.pasha@utoronto.ca>
2014-12-31 15:55:51 -0500

    Worked on HTML and CSS.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,16 @@
+/*global cytoscape*/
 (function(window, $, cytoscape, undefined) {
 	'use strict';
 
 	window.addEventListener('Agave::ready', function() {
-		window.alert("JS file ready.");
+		//window.alert('JS file ready.');
+	});
+
+	$(document).ready(function() {
+		// About button
+		$('#about').click(function() {
+			window.alert('This app was developed by the BAR team with help from the AIP team. The data is obtained from BAR databases using webservices.');
+		});
 	});
 
 })(window, jQuery, cytoscape);
