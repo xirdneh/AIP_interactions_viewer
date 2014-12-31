a6dd72140cde5fe609d0ca146a373789c8b75fe6
Asher <asher.pasha@utoronto.ca>
2014-12-31 15:59:47 -0500

    Worked on about button.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,13 +4,12 @@
 
 	window.addEventListener('Agave::ready', function() {
 		//window.alert('JS file ready.');
-	});
 
-	$(document).ready(function() {
-		// About button
-		$('#about').click(function() {
-			window.alert('This app was developed by the BAR team with help from the AIP team. The data is obtained from BAR databases using webservices.');
+		$(document).ready(function() {
+			// About button
+			$('#about').click(function() {
+				window.alert('This app was developed by the BAR team with help from the AIP team. The data is obtained from BAR databases using webservices.');
+			});
 		});
 	});
-
 })(window, jQuery, cytoscape);
