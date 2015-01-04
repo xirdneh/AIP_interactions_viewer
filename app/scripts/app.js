9cfd328d2e1f4069aef6c1fc85d0b0deba733183
Asher <asher.pasha@utoronto.ca>
2015-01-03 21:49:55 -0500

    Cystoscape mostly working, but still hardcoded.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,24 +2,32 @@
 (function(window, $, cytoscape, undefined) {
 	'use strict';
 
+	var appContext = $('[data-app-name="aip-interactions-viewer"]');
+
+	// Variables for graph
+	var nodes = [], edges = [];
+
 	window.addEventListener('Agave::ready', function() {
-		//window.alert('JS file ready.');
+		var Agave = window.Agave;
 
 		// Variable
-		var el, i, cytoscapeJsUrl, reCytoscape, hasCytoscape, allScripts;
+		var el, i, cytoscapeJsUrl, arborJsUrl, reCytoscape, reArbor, hasCytoscape, hasArbor, allScripts;
 
 		// Intialize
 		// This is code from old AIP BAR interactions viewer (not sure if the app would work without it!)
 		function init() {
 			cytoscapeJsUrl = 'bower_components/cytoscape/dist/cytoscape.min.js';	// The cytoscape.js 
+			arborJsUrl = 'bower_components/cytoscape/lib/arbor.js';	// The layout file
 
-			hasCytoscape = false;
+			hasCytoscape = hasArbor = false;
 			reCytoscape = new RegExp(cytoscapeJsUrl);
+			reArbor = new RegExp(arborJsUrl);
 			allScripts = document.querySelectorAll('script');
 
 			// This is checking if cytoscape is present
 			for (i = 0; i < allScripts.length && !(hasCytoscape); i++) {
 				hasCytoscape = hasCytoscape || reCytoscape.test(allScripts[i].src);
+				hasArbor = hasArbor || reCytoscape.test( allScripts[i].src );
 			}
 
 			// If we don't have cytoscape, add it
@@ -29,60 +37,91 @@
 				el.type = 'text/javascript';
 				document.body.appendChild(el);
 			}
+
+			// If we don't have Arbor, add it
+			if (!hasArbor) {
+				el = document.createElement( 'script' );
+				el.src = arborJsUrl;
+				el.type = 'text/javascript';
+				document.body.appendChild( el );
+			}
 		}
 		init();
+
 
 		$(document).ready(function() {
 			// This function loads cytoscape. 'elements' stores the newtwork
 			var loadCy = function(elements) {
+				var myStyle = 'node { content: data(name);}';
 				$('#cyto').removeClass('hidden').cytoscape({
 					layout: {
-						name: 'grid',
-						fit: false,
-						padding: 50,
-						avoidOverlap: true,
-						position: function(node){},
-						animate: false,
-						animationDuration: 500,
+						name: 'arbor',
+						liveUpdate: true,
+						maxSimulationTime: 4000,
+						padding: [ 50, 50, 50, 50 ],
+						ungrabifyWhileSimulating: true,
+						gravity: true,
+						stepSize: 1
 					},
-					style: cytoscape.stylesheet()
-						.selector('node')
-							.css({
-								'shape': 'circle',
-								'width': 30,
-								'height': 30,
-								'content': 'data(id)',
-								'color': 'white',
-								'text-outline-width': 2,
-								'text-outline-color': '#888'
-							})
-						.selector('edge')
-							.css({
-								'width': 'data(width)',
-								'line-style': 'data(lineStyle)',
-								'line-color': 'data(color)',
-							}),
+					style: myStyle, 					
 					elements: elements
 				}); // End .cytoscape()
 			}; //  End loadCy()
 
 			// Reset button
-			$('#interactions-form').on('reset', function() {
+			$('#interactions-form', appContext).on('reset', function() {
 				document.forms['interactions-form'].loci.value = '';
 				$('#cyto').addClass('hidden');
 			});
 
 			// Submit button
-			$('#interactions-form').on('submit', function(e) {
+			$('#interactions-form', appContext).on('submit', function(e) {
 				e.preventDefault();
-				var data = $('#loci').val();
-				window.alert('data: ' + data);
+				
+				//var loci = $('#loci').val();
+
+				// Add query to nodes
+				
+				var query = {
+					locus: 'AT1G01010',
+					published: 'false'
+				};
+
+				Agave.api.adama.search({
+					'namespace': 'asher', 'service': 'interactions_v0.1', 'queryParams': query
+				}, function(response) {
+					nodes = [];
+					edges = [];
+					nodes.push({data: {
+						id: 'AT1G01010',
+						name: 'AT1G01010'
+					}});
+
+					for (var i = 0; i < response.obj.result.length; i++) {
+						nodes.push({data: {
+							id: response.obj.result[i].locus,
+							name: response.obj.result[i].locus
+						}});
+					
+						edges.push({data: {
+							source: 'AT1G01010', 
+							target: response.obj.result[i].locus
+						}});
+					}
+		
+					var elements = {nodes: nodes, edges: edges}; 
+					loadCy(elements);
+				});
+
 			});					
 
 			// About button
 			$('#about').click(function() {
 				window.alert('This app was developed by the BAR team with help from the AIP team. The data is obtained from BAR databases using webservices.');
 			});
+
+
+
 		});
 	});
 })(window, jQuery, cytoscape);
